7fab92131994e5d0540e1c35f593f99965faed6c
Jacob Carey <jacobcvt12@gmail.com>
2015-08-09 10:59:48 -0400

    Update theme and plugin usage in config.fish

--- config.fish
+++ config.fish
@@ -1,17 +1,17 @@
 # Path to your oh-my-fish.
 set fish_path ~/dotfiles/.oh-my-fish
 
-# Theme
-set fish_theme robbyrussell
-
-# Plugins
-set fish_plugins vundle 
-
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
 # Load autojump
 . /usr/local/etc/autojump.fish
+
+# Theme
+Theme "robbyrussell"
+
+# Plugins
+Plugin "vundle"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
