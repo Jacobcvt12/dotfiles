b0dbbf18e811625ae8ee35fa381d0d6343801ec9
Jacob Carey <jacobcvt12@gmail.com>
2015-09-12 16:06:01 -0400

    Install brew packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
-if ! hash brew 2>/dev/null; then
+# if brew not installed and OS X then install brew
+if [ ! hash brew 2>/dev/null ] && [ "$(uname)" == "Darwin" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+fi
+
+# if OS X then install brew taps and packages
+if [ "$(uname)" == "Darwin" ]; then
+    cat ./homebrew/taps.txt | brew tap
+    cat ./homebrew/leaves.txt | brew install
 fi
