d1eb2d6421452e08b9f5cc1b1962e3ad0053f4dc
Jacob Carey <jacobcvt12@gmail.com>
2015-09-12 15:54:29 -0400

    Correct homebrew check.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-if hash brew 2>/dev/null; then
+if ! hash brew 2>/dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
