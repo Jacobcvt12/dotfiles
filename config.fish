a2aa6992e5872f0922902e8900c992ff4e856a20
Jacob Carey <jacobcvt12@gmail.com>
2015-11-04 21:56:26 -0500

    Update oh my fish framework

--- config.fish
+++ config.fish
@@ -1,11 +1,11 @@
-# Path to your oh-my-fish.
-set fish_path ~/dotfiles/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/jacobcvt12/.local/share/omf
 
 # add latex to PATH
 set -x PATH $PATH /Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+source $OMF_PATH/init.fish
 
 # Load autojump
 . /usr/local/etc/autojump.fish
