42dcb1cb5beb339c95aa0b45defed35eed0d0843
Jacob Carey <jacobcvt12@gmail.com>
2017-04-01 21:10:08 -0400

    Add alias for julia-dev

--- config.fish
+++ config.fish
@@ -4,5 +4,8 @@ set -gx OMF_PATH /Users/jacobcvt12/.local/share/omf
 # add latex to PATH
 set -x PATH $PATH /Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin
 
+# alias for julia dev binary
+alias julia-dev="/Users/jacobcvt12/Code/julia/julia"
+
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
